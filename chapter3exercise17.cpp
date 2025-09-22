ae132c160a4f1dfe3abe9ff7578a8566ae2b7944
ctpride07 <ctpride07@gmail.com>
2025-09-21 20:10:38 -0400

    Add project files.

---
+++ chapter3exercise17.cpp
@@ -0,0 +1,12 @@
+/*
+* File Name: chapter3exercise6
+* Programmer: Carter Pride
+* Date: 09/2025
+* Purpose to determine the amount of ingrediants needed to bake a desired amount of cookies.
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
