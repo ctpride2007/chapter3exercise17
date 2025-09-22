4663ca1831e96e1aa6722d54e13265e9109c8899
ctpride07 <ctpride07@gmail.com>
2025-09-21 20:16:06 -0400

    Replace Hello World with random number generator
    Updated the program to generate and display two random numbers between 100 and 999 as a math problem. Added necessary headers for random number generation and initialized the random seed. Fixed a typo in the random number function from `rant()` to `rand()`.

--- chapter3exercise17.cpp
+++ chapter3exercise17.cpp
@@ -5,8 +5,30 @@
 * Purpose to determine the amount of ingrediants needed to bake a desired amount of cookies.
 */
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(0));
+
+	int num1 = rant() % 900 + 100, num2 = rand() % 900 + 100;
+
+	cout << " Solve the displayed problem. \n ";
+	cout << "  " << num1 << endl;
+	cout << "+ " << num2 << endl;
+	cout << "----" << endl;
+
+	cout << "\nPress enter to show answer.";
+	cin.ignore();
+	cin.get();
+
+	int sum = num1 + num2;
+
+	cout << "\n  " << num1 << endl;
+	cout << "+ " << num2 << endl;
+	cout << "----" << endl;
+	cout << "  " << sum << endl;
+
 }
